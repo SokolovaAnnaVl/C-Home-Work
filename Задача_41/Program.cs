e0f694c3053e34ab1ae8459b1eb03252bc3469c1
Sokolova  Anna <124304899+SokolovaAnnaVl@users.noreply.github.com>
2023-03-08 01:15:05 +0800

    Add files via upload

---
+++ Задача_41/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int GetPositiv(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите М чисел: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = GetArray(size, -100, 100);
+
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine(" -> ");
+Console.WriteLine(String.Join(", ", GetPositiv(array)));
---
+++ Задача_43/Program.cs
@@ -0,0 +1,42 @@
+﻿// // Задача 43: Напишите программу, которая найдёт точку пересечения 
+// двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// y=k1*x+b1, y=k2*x+b2
+// k1*x+b1=k2*x+b2
+// (k1-k2)*x=b2-b1
+// x=(b2-b1)/(k1-k2)
+// y=k1*x+b1
+
+Console.Clear();
+
+double ResultX(int b1, int k1, int b2, int k2)
+{
+    double x1 = b2 - b1;
+    double x2 = k1 - k2;
+    double x = x1 / x2;
+    return x;
+}
+
+
+double ResultY(int k1, int b1, double x)
+{
+    double y = (k1 * x) + b1;
+    return y;
+}
+
+Console.WriteLine("Введите значение b1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение k1: ");
+int k1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение b2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение k2: ");
+int k2 = int.Parse(Console.ReadLine()!);
+
+double x = ResultX(b1, k1, b2, k2);
+double y = ResultY(k1, b1, x);
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} > ({x}; {y})");
