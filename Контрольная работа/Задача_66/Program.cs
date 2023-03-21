6894dfa8d9260665243a626e62d889f14b663a70
Sokolova  Anna <124304899+SokolovaAnnaVl@users.noreply.github.com>
2023-03-22 01:18:51 +0800

    Контрольная работа по C#

---
+++ Контрольная работа/Задача_66/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void SumNatural(int M, int N)
+{
+    Console.Write(SumMN(M - 1, N));
+}
+
+int SumMN(int M, int N)
+{
+    int result = M;
+    if (M == N)
+        return 0;
+    else
+    {
+        M++;
+        result = M + SumMN(M, N);
+        return result;
+    }
+}
+
+Console.Clear();
+
+Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write($"Сумма натуральных элементов в промежутке от M до N = {SumMN(M, N) + M}");
+Console.WriteLine();
---
+++ Контрольная работа/Задача_68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью 
+// рекурсии. Даны два неотрицательных числа m и n. 
+// m = 2, n = 3 -> A(m,n) = 9
+
+Console.Clear();
+
+int Akkerman(int M, int N)
+{
+if (M == 0) return N + 1;
+else if (N == 0) return Akkerman(M - 1, 1);
+else return Akkerman(M - 1, Akkerman(M, N - 1));
+}
+
+Console.WriteLine("Введите значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write($"Функция Аккермана (рекурсия) = {Akkerman(M, N)}");
+Console.WriteLine();
